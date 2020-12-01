9df8f87b42479b55e4a37a7ffd21313b0dd73fd1
Lukas <games.luki@gmail.com>
2020-12-01 17:21:04 +0100

    Updated project structure

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+import itertools
+
+from problem import Problem
+
+problem = Problem(1)
+data = problem.get_data('int')
+
+# Part a
+comb = itertools.combinations(data, 2)
+comb = [(a, b) for a, b in comb if a + b == 2020]
+answer = comb[0][0] * comb[0][1]
+
+problem.submit(answer, 'a')
+
+# Part b
+comb = itertools.combinations(data, 3)
+comb = [(a, b, c) for a, b, c in comb if a + b + c == 2020]
+answer = comb[0][0] * comb[0][1] * comb[0][2]
+
+problem.submit(answer, 'b')
---
+++ problem.py
@@ -0,0 +1,38 @@
+from aocd import get_data, submit
+
+
+class Problem:
+    def __init__(self, day: int, store_input=True):
+        self._day = day
+        self._data = get_data(day=day).split('\n')
+
+        if store_input:
+            with open('input.txt', 'w') as fh:
+                fh.writelines('\n'.join(self._data))
+
+    def get_data(self, dtype='str'):
+        if dtype == 'str':
+            return self._data
+        elif dtype == 'int':
+            return [int(line) for line in self._data]
+        elif dtype == 'float':
+            return [float(line) for line in self._data]
+
+    def data(self, dtype='str'):
+        for line in self._data:
+            if dtype == 'str':
+                yield line
+            elif dtype == 'int':
+                yield int(line)
+            elif dtype == 'float':
+                yield float(line)
+
+    def submit(self, answer, part: str, store_answer=True):
+        submit(answer, part=part, day=self._day)
+
+        if store_answer:
+            with open('answer.txt', 'w') as fh:
+                if isinstance(answer, list):
+                    fh.writelines('\n'.join(str(answer)))
+                else:
+                    fh.write(str(answer))
