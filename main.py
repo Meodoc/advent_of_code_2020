1678c40fcb6104d8ef5d09239417f09ff0a88cde
Lukas <games.luki@gmail.com>
2020-12-01 11:24:35 +0100

    Day 1, part 2: slight improvement

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import itertools
 
 with open("day1/input", "r") as fh:
-    lines = fh.readlines()
+    lines = [int(l) for l in fh.readlines()]
 
 comb = itertools.combinations(lines, 3)
-comb = [(int(a), int(b), int(c)) for a, b, c in comb if int(a) + int(b) + int(c) == 2020]
+comb = [(a, b, c) for a, b, c in comb if a + b + c == 2020]
 print(comb[0][0] * comb[0][1] * comb[0][2])
