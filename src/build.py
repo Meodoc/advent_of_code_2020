ed01e8f2be0d2d64705c2b2f8701152555908757
Lukas <games.luki@gmail.com>
2020-12-03 14:32:12 +0100

    Refactoring

--- src/build.py
+++ src/build.py
@@ -12,7 +12,7 @@ def create_day_stub(day: int, create_test_file: bool):
     # Create day.py
     override_day = True
     if os.path.exists(file_name := f'day{day}/day{day}.py'):
-        override_day = _do_override(file_name)
+        override_day = _prompt_override(file_name)
     if override_day:
         with open(f'day{day}/day{day}.py', 'w') as fh:
             fh.write("from src.problem import Problem\n\n\n")
@@ -26,13 +26,13 @@ def create_day_stub(day: int, create_test_file: bool):
     # Create test.in (optional)
     if create_test_file:
         if os.path.exists(file_name := f'day{day}/test.in'):
-            if not _do_override(file_name):
+            if not _prompt_override(file_name):
                 return
         with open(f'day{day}/test.in', 'w'):
             pass
 
 
-def _do_override(file_name: str):
+def _prompt_override(file_name: str):
     while (x := input(f"{file_name} already exists. Do you want to override? (y/n): ",)) != 'y' and x != 'n':
         print("Invalid input, try again.")
     return x == 'y'
