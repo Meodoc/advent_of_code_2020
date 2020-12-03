c90360748f18d2c7375c40bbf3dcdc512bc6c71c
Lukas <games.luki@gmail.com>
2020-12-03 09:11:27 +0100

    Day3, part b: small improvement

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -14,28 +14,24 @@ def part_a():
 
 def part_b():
     n_trees = [0, 0, 0, 0, 0]
-    c0 = 0
-    c1 = 0
-    c2 = 0
-    c3 = 0
-    c4 = 0
+    cols = [0, 0, 0, 0, 0]
     for r, line in enumerate(data):
-        if line[c0] == '#':
+        if line[cols[0]] == '#':
             n_trees[0] += 1
-        c0 = (c0 + 1) % len(line)
-        if line[c1] == '#':
+        cols[0] = (cols[0] + 1) % len(line)
+        if line[cols[1]] == '#':
             n_trees[1] += 1
-        c1 = (c1 + 3) % len(line)
-        if line[c2] == '#':
+        cols[1] = (cols[1] + 3) % len(line)
+        if line[cols[2]] == '#':
             n_trees[2] += 1
-        c2 = (c2 + 5) % len(line)
-        if line[c3] == '#':
+        cols[2] = (cols[2] + 5) % len(line)
+        if line[cols[3]] == '#':
             n_trees[3] += 1
-        c3 = (c3 + 7) % len(line)
+        cols[3] = (cols[3] + 7) % len(line)
         if r % 2 == 0:
-            if line[c4] == '#':
+            if line[cols[4]] == '#':
                 n_trees[4] += 1
-            c4 = (c4 + 1) % len(line)
+            cols[4] = (cols[4] + 1) % len(line)
 
     return n_trees[0] * n_trees[1] * n_trees[2] * n_trees[3] * n_trees[4]
 
@@ -43,6 +39,7 @@ def part_b():
 if __name__ == '__main__':
     problem = Problem(3)
     data = problem.get_data()
-
+    
+    print(part_b())
     problem.submit(part_a(), 'a')
     problem.submit(part_b(), 'b')
