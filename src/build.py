1935220a18c07156a412864fff48b4c4dc96a3ec
Lukas <games.luki@gmail.com>
2020-12-04 14:23:45 +0100

    Updated build.py

--- src/build.py
+++ src/build.py
@@ -6,29 +6,32 @@ import os
 @click.option('-d', '--day', required=True, type=int, help='Day you want to create')
 @click.option('-t', '--create-test-file', is_flag=True, help='Create a test file')
 def create_day_stub(day: int, create_test_file: bool):
-    if not os.path.exists(f'day{day}'):
-        os.mkdir(f'day{day}')
+    if not os.path.exists(f'src/day{day}'):
+        os.mkdir(f'src/day{day}')
 
     # Create day.py
     override_day = True
-    if os.path.exists(file_name := f'day{day}/day{day}.py'):
+    if os.path.exists(file_name := f'src/day{day}/day{day}.py'):
         override_day = _prompt_override(file_name)
     if override_day:
-        with open(f'day{day}/day{day}.py', 'w') as fh:
+        with open(f'src/day{day}/day{day}.py', 'w') as fh:
             fh.write("from src.problem import Problem\n\n\n")
             fh.write("def part_a():\n    return None\n\n\n")
             fh.write("def part_b():\n    return None\n\n\n")
+            fh.write("def load():\n    data = problem.data()\n")
+            fh.write("    return data\n\n\n")
             fh.write("if __name__ == '__main__':\n")
-            fh.write(f"    problem = Problem({day})\n\n")
+            fh.write(f"    problem = Problem({day})\n")
+            fh.write("    data = load()\n\n")
             fh.write("    # problem.submit(part_a(), 'a')\n")
             fh.write("    # problem.submit(part_b(), 'b')\n")
 
     # Create test.in (optional)
     if create_test_file:
-        if os.path.exists(file_name := f'day{day}/test.in'):
+        if os.path.exists(file_name := f'src/day{day}/test.in'):
             if not _prompt_override(file_name):
                 return
-        with open(f'day{day}/test.in', 'w'):
+        with open(f'src/day{day}/test.in', 'w'):
             pass
 
 
