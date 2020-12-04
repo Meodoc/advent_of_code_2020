82b762729b663809ee28b1a78a78da160fe63cbe
Lukas <games.luki@gmail.com>
2020-12-04 14:19:07 +0100

    Updated data loading in problem.py

--- src/problem.py
+++ src/problem.py
@@ -4,25 +4,28 @@ from aocd import get_data, submit
 class Problem:
     def __init__(self, day: int, store_input=True):
         self._day = day
-        self._data = get_data(day=day).split('\n\n')
+        self._data = get_data(day=day)
 
         if store_input:
             with open('input.in', 'w') as fh:
                 fh.writelines('\n'.join(self._data))
 
-    def get_data(self, dtype=str):
-        return [dtype(line) for line in self._data]
+    def raw_data(self):
+        return self._data
 
-    def data(self, dtype=str):
-        for line in self._data:
-            yield dtype(line)
+    # def data(self, delim='\n', dtype=str):
+    #     for line in self._data.split(delim):
+    #         yield dtype(line)
 
-    def get_test_input(self, dtype=str):
-        return [dtype(line) for line in self._open_test_input()]
+    def data(self, delim='\n', dtype=str):
+        return [dtype(line) for line in self._data.split(delim)]
 
-    def test_input(self, dtype=str):
-        for line in self._open_test_input():
-            yield dtype(line)
+    # def test_data(self, delim='\n', dtype=str):
+    #     for line in self._raw_test_input().split(delim):
+    #         yield dtype(line)
+
+    def test_data(self, delim='\n', dtype=str):
+        return [dtype(line) for line in self._raw_test_input().split(delim)]
 
     def submit(self, answer, part: str, store_answer=True):
         submit(answer, part=part, day=self._day)
@@ -35,7 +38,7 @@ class Problem:
                     fh.write(str(answer))
 
     # -- Helpers --
-    def _open_test_input(self):
+    def _raw_test_input(self):
         with open('test.in', 'r') as fh:
-            return "".join(fh.readlines()).split('\n\n')
+            return "".join(fh.readlines())
 
