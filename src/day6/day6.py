d82a1f59b7a47f0c4b2bd405bf222b0842ac866f
Lukas <games.luki@gmail.com>
2020-12-06 12:34:32 +0100

    Day6, part a: oneliner

--- src/day6/day6.py
+++ src/day6/day6.py
@@ -5,17 +5,7 @@ import re
 
 # TODO: weird interaction when dict without indices
 def part_a():
-    yes_answers = dict()
-    for i, group in enumerate(data):
-        yes_answers[i] = set()
-        for person in group.split('\n'):
-            for yes_answer in person:
-                yes_answers[i].add(yes_answer)
-    result = 0
-    for i, _ in enumerate(yes_answers):
-        result += len(yes_answers[i])
-
-    return result
+    return sum([len(answer) for answer in [set([answer for person in group.split('\n')for answer in person]) for group in data]])
 
 
 def part_b():
@@ -28,11 +18,13 @@ def part_b():
             for yes_answer in person:
                 person_answers[j].add(yes_answer)
 
-        yes_answers[i] = set.intersection(*[x for x in person_answers.values()])
+        yes_answers[i] = set.intersection(*person_answers.values())
 
     result = 0
     for i, _ in enumerate(yes_answers):
         result += len(yes_answers[i])
+
+
     return result
 
 
@@ -44,7 +36,11 @@ if __name__ == '__main__':
     problem = Problem(6)
     data = load()
 
+
+    print(part_a())
     # 6624 too low
     print(part_b())  # 6625
     #problem.submit(part_a(), 'a')
-    problem.submit(part_b(), 'b')
+    #problem.submit(part_b(), 'b')
+
+
