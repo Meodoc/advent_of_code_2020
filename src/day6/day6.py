5bca88ab4bf22384818e215a365ae3a722ddf1b3
Lukas <games.luki@gmail.com>
2020-12-06 12:50:57 +0100

    Day6, part b: oneliner

--- src/day6/day6.py
+++ src/day6/day6.py
@@ -1,31 +1,12 @@
 from src.problem import Problem
 
-import re
 
-
-# TODO: weird interaction when dict without indices
 def part_a():
-    return sum([len(answer) for answer in [set([answer for person in group.split('\n')for answer in person]) for group in data]])
+    return sum([len(answer) for answer in [set([answer for person in group.split('\n') for answer in person]) for group in data]])
 
 
 def part_b():
-    yes_answers = dict()
-    for i, group in enumerate(data):
-        yes_answers[i] = set()
-        person_answers = dict()
-        for j, person in enumerate(group.split('\n')):
-            person_answers[j] = set()
-            for yes_answer in person:
-                person_answers[j].add(yes_answer)
-
-        yes_answers[i] = set.intersection(*person_answers.values())
-
-    result = 0
-    for i, _ in enumerate(yes_answers):
-        result += len(yes_answers[i])
-
-
-    return result
+    return sum([len(answer) for answer in [set.intersection(*[set(answer for answer in person) for person in group.split('\n')]) for group in data]])
 
 
 def load():
@@ -38,9 +19,8 @@ if __name__ == '__main__':
 
 
     print(part_a())
-    # 6624 too low
-    print(part_b())  # 6625
-    #problem.submit(part_a(), 'a')
+    print(part_b())
+    #problem.submit(part_a(), 'a')  # 6625
     #problem.submit(part_b(), 'b')
 
 
