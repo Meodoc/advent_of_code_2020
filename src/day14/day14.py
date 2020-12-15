dacb5e72d0cef9f60aa6a28228253e4832fc02f5
Lukas <games.luki@gmail.com>
2020-12-15 01:31:27 +0100

    Day14: part b

--- src/day14/day14.py
+++ src/day14/day14.py
@@ -1,11 +1,14 @@
 from src.problem import Problem
 
 from dataclasses import dataclass
+from itertools import product
 import re
 
 # Commands
 UPDATE_MASK = 'mask'
 STORE = 'mem'
+
+BITMASK_LEN = 36
 
 PATTERNS = [re.compile(fr'^({UPDATE_MASK}) = ([X|\d]+)'),
             re.compile(fr'^({STORE})\[(\d+)] = (\d+)')]
@@ -22,7 +25,7 @@ class Decoder:
         elif cmd == STORE:
             self._store(*args)
 
-    def _store(self, adr: int, val: int) -> None:
+    def _store(self, adr: str, val: str) -> None:
         self.memory[adr] = (int(val) | self.bitmask[0]) & self.bitmask[1]
 
     def _update_mask(self, mask: str) -> None:
@@ -33,6 +36,31 @@ class Decoder:
         return sum(self.memory.values())
 
 
+class QuantumDecoder(Decoder):
+    bitmask: int
+    q_indices: list
+
+    def _store(self, adr: str, val: str) -> None:
+        for p in product([0, 1], repeat=len(self.q_indices)):
+            q_adr = self.int_to_bin_str(int(adr), BITMASK_LEN)
+            for i in range(len(self.q_indices)):
+                q_adr = self.replace_at_index(q_adr, str(p[i]), self.q_indices[i])
+            q_adr = int(q_adr, 2) | self.bitmask
+            self.memory[q_adr] = int(val)
+
+    def _update_mask(self, mask: str) -> None:
+        self.bitmask = int(mask.replace('X', '0'), 2)
+        self.q_indices = [f.start() for f in re.finditer('X', mask)]
+
+    @staticmethod
+    def int_to_bin_str(val: int, l: int):
+        return format(val, 'b').zfill(l)
+
+    @staticmethod
+    def replace_at_index(s: str, r: str, idx: int):
+        return s[:idx] + r + s[idx + 1:]
+
+
 def part_a():
     decoder = Decoder()
     for cmd, args in data:
@@ -41,7 +69,10 @@ def part_a():
 
 
 def part_b():
-    return None
+    decoder = QuantumDecoder()
+    for cmd, args in data:
+        decoder.interpret(cmd, args)
+    return decoder.calc_sum()
 
 
 def load():
@@ -54,6 +85,5 @@ if __name__ == '__main__':
     problem = Problem(14)
     data = load()
 
-    print(part_a())
-    problem.submit(part_a(), 'a')
-    # problem.submit(part_b(), 'b')
+    problem.submit(part_a(), 'a')  # 6513443633260
+    problem.submit(part_b(), 'b')  # 3442819875191
