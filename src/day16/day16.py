f8e2bc37384a62c7a4de95533566013b1e4d5625
Lukas <games.luki@gmail.com>
2020-12-17 20:59:48 +0100

    Day16: part b

--- src/day16/day16.py
+++ src/day16/day16.py
@@ -1,5 +1,7 @@
 from src.problem import Problem
 
+from copy import deepcopy
+from math import prod
 import re
 
 RULE_PATTERN = re.compile(r'^([a-z|\s]+): (\d+)-(\d+) or (\d+)-(\d+)')
@@ -10,16 +12,37 @@ def _rule_closure(min1, max1, min2, max2):
 
 
 def part_a():
-    rules = {(m := RULE_PATTERN.match(line)).group(1): _rule_closure(m.group(2), m.group(3), m.group(4), m.group(5))
-             for line in data[0]}
-
     # Return the sum of all fields that match no rule
     return sum(int(field) for ticket in data[2] for field in ticket.split(',')
                if all(not rules[k](field) for k, _ in rules.items()))
 
 
 def part_b():
-    return None
+    # Filters for the tickets where every field fulfills at least one rule
+    valid_tickets = [ticket for ticket in data[2]
+                     if all(any(rules[r](field) for r, _ in rules.items()) for field in ticket.split(','))]
+
+    # Algorithm finds the positions of all the fields and stores them in a dict
+    rule_positions = dict()
+    while len(rule_positions) < (n_fields := len(data[1].split(','))):
+        for pos in range(n_fields):
+            applying_rule = None
+            for r, _ in rules.items():
+                # Check if rule applies for every ticket at current position 'pos'
+                if all(rules[r](ticket.split(',')[pos]) for ticket in valid_tickets):
+                    if applying_rule:  # If more then one rule applies to the position, skip this position for now
+                        applying_rule = None
+                        break
+                    applying_rule = (r, pos)
+            # If exactly one rule applies, save the position of that rule and remove it from the available rules
+            if applying_rule:
+                rule_positions[applying_rule[0]] = applying_rule[1]
+                rules.pop(applying_rule[0])
+                break
+
+    # Returns the product of all fields in own ticket that start with 'departure'
+    return prod(int(field) for pos, field in enumerate(data[1].split(',')) if pos in
+                [r_pos for rule, r_pos in rule_positions.items() if rule.startswith('departure')])
 
 
 def load():
@@ -33,6 +56,8 @@ if __name__ == '__main__':
     problem = Problem(16)
     data = load()
 
-    print(part_a())
+    rules = {(m := RULE_PATTERN.match(line)).group(1): _rule_closure(m.group(2), m.group(3), m.group(4), m.group(5))
+             for line in data[0]}
+
     problem.submit(part_a(), 'a')  # 20091
-    # problem.submit(part_b(), 'b')
+    problem.submit(part_b(), 'b')  # 2325343130651
