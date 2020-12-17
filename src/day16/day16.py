84e6a11b9e109f6641b7afeda82a8ac661cc89ff
Lukas <games.luki@gmail.com>
2020-12-17 22:03:10 +0100

    Day16: part b: refactoring

--- src/day16/day16.py
+++ src/day16/day16.py
@@ -1,6 +1,5 @@
 from src.problem import Problem
 
-from copy import deepcopy
 from math import prod
 import re
 
@@ -28,25 +27,29 @@ def part_b():
     #  be added to the 'rule_positions' dict. Then the search for the next rule begins again from the position 0.
     #  The algorithm is finished once it finds a position for every rule.
     rule_positions = dict()
-    while len(rule_positions) < (n_fields := len(data[1].split(','))):
-        for pos in range(n_fields):
-            applying_rule = None
-            for r, _ in rules.items():
-                # Check if rule applies for every ticket at current position 'pos'
-                if all(rules[r](ticket.split(',')[pos]) for ticket in valid_tickets):
-                    if applying_rule:  # If more then one rule applies to the position, skip this position for now
-                        applying_rule = None
-                        break
-                    applying_rule = (r, pos)
-            # If exactly one rule applies, save the position of that rule and remove it from the available rules
-            if applying_rule:
-                rule_positions[applying_rule[0]] = applying_rule[1]
-                rules.pop(applying_rule[0])
-                break
+    while len(rule_positions) < (len(data[1].split(','))):
+        rule, pos = find_unique_rule(rules, valid_tickets)
+        rule_positions[rule] = pos
+        rules.pop(rule)
 
     # Returns the product of all fields in own ticket that start with 'departure'
     return prod(int(field) for pos, field in enumerate(data[1].split(',')) if pos in
                 [r_pos for rule, r_pos in rule_positions.items() if rule.startswith('departure')])
+
+
+def find_unique_rule(rules: dict, tickets: list):
+    for pos in range(len(tickets[0].split(','))):
+        applying_rule = None
+        for r, _ in rules.items():
+            # Check if rule applies for every ticket at current position 'pos'
+            if all(rules[r](ticket.split(',')[pos]) for ticket in tickets):
+                if applying_rule:  # If more then one rule applies to the position, skip the position
+                    applying_rule = None
+                    break
+                applying_rule = (r, pos)
+        # If unique rule was found, return it
+        if applying_rule:
+            return applying_rule
 
 
 def load():
@@ -63,5 +66,7 @@ if __name__ == '__main__':
     rules = {(m := RULE_PATTERN.match(line)).group(1): _rule_closure(m.group(2), m.group(3), m.group(4), m.group(5))
              for line in data[0]}
 
+    print(part_b())
+
     problem.submit(part_a(), 'a')  # 20091
-    problem.submit(part_b(), 'b')  # 2325343130651
+    #problem.submit(part_b(), 'b')  # 2325343130651
