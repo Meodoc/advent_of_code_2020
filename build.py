a293be1442e2b48b378ca4306fede204899f3e26
Lukas <games.luki@gmail.com>
2020-12-30 23:53:04 +0100

    Refactoring and bugfix

--- build.py
+++ build.py
@@ -6,39 +6,43 @@ import os
 @click.option('-d', '--day', required=True, type=int, help='Day you want to create')
 @click.option('-t', '--create-test-file', is_flag=True, help='Create a test file')
 def create_day_stub(day: int, create_test_file: bool):
-    if not os.path.exists(day_src := f'src/{day}'):
-        os.mkdir(day_src)
-    if not os.path.exists(day_data := f'data/{day}'):
-        os.mkdir(day_data)
+    src_path = f'src/{day:02}'
+    data_path = f'data/{day:02}'
+    file_path = f'{src_path}/day{day}.py'
+
+    if not os.path.exists(src_path):
+        os.mkdir(src_path)
+    if not os.path.exists(data_path):
+        os.mkdir(data_path)
 
     # Create day.py
     override_day = True
-    if os.path.exists(file_name := f'src/{day}/day{day}.py'):
-        override_day = _prompt_override(file_name)
+    if os.path.exists(file_path):
+        override_day = _prompt_override(file_path)
     if override_day:
-        with open(f'src/{day}/day{day}.py', 'w') as fh:
-            fh.write("from src.problem import Problem\n\n\n")
-            fh.write("def part_a(data: list):\n    return None\n\n\n")
-            fh.write("def part_b(data: list):\n    return None\n\n\n")
-            fh.write("def load(p: Problem):\n    return p.data()\n\n\n")
-            fh.write("if __name__ == '__main__':\n")
-            fh.write(f"    problem = Problem({day})\n\n")
-            fh.write("    print(part_a(load(problem)))\n")
-            fh.write("    # print(part_b(load(problem)))\n\n")
-            fh.write("    # problem.submit(part_a(load(problem)), 'a')\n")
-            fh.write("    # problem.submit(part_b(load(problem)), 'b')\n")
+        with open(file_path, 'w') as fh:
+            fh.write("from src.problem import Problem\n\n\n"
+                     "def part_a(data: list):\n    return None\n\n\n"
+                     "def part_b(data: list):\n    return None\n\n\n"
+                     "def load(p: Problem):\n    return p.data()\n\n\n"
+                     "if __name__ == '__main__':\n"
+                     f"    problem = Problem({day})\n\n"
+                     "    print(part_a(load(problem)))\n"
+                     "    # print(part_b(load(problem)))\n\n"
+                     "    # problem.submit(part_a(load(problem)), 'a')\n"
+                     "    # problem.submit(part_b(load(problem)), 'b')\n")
 
     # Create test.in (optional)
     if create_test_file:
-        if os.path.exists(file_name := f'src/{day}/test.in'):
-            if not _prompt_override(file_name):
+        if os.path.exists(test_path := f'{src_path}/test.in'):
+            if not _prompt_override(test_path):
                 return
-        with open(f'src/{day}/test.in', 'w'):
+        with open(test_path, 'w'):
             pass
 
 
 def _prompt_override(file_name: str):
-    while (x := input(f"{file_name} already exists. Do you want to override? (y/n): ",)) != 'y' and x != 'n':
+    while (x := input(f"{file_name} already exists. Do you want to override? (y/n): ", )) != 'y' and x != 'n':
         print("Invalid input, try again.")
     return x == 'y'
 
