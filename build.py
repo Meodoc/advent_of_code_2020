d1401309ccf008fc69ddcffd837f86e56a81584d
Lukas <games.luki@gmail.com>
2020-12-30 21:44:14 +0100

    Fixed bugs in build.py and updated to new folder names

--- build.py
+++ build.py
@@ -6,17 +6,17 @@ import os
 @click.option('-d', '--day', required=True, type=int, help='Day you want to create')
 @click.option('-t', '--create-test-file', is_flag=True, help='Create a test file')
 def create_day_stub(day: int, create_test_file: bool):
-    if not os.path.exists(day_src := f'src/day{day}'):
+    if not os.path.exists(day_src := f'src/{day}'):
         os.mkdir(day_src)
-    if not os.path.exists(day_data := f'data/day{day}'):
+    if not os.path.exists(day_data := f'data/{day}'):
         os.mkdir(day_data)
 
     # Create day.py
     override_day = True
-    if os.path.exists(file_name := f'src/day{day}/day{day}.py'):
+    if os.path.exists(file_name := f'src/{day}/day{day}.py'):
         override_day = _prompt_override(file_name)
     if override_day:
-        with open(f'src/day{day}/day{day}.py', 'w') as fh:
+        with open(f'src/{day}/day{day}.py', 'w') as fh:
             fh.write("from src.problem import Problem\n\n\n")
             fh.write("def part_a(data: list):\n    return None\n\n\n")
             fh.write("def part_b(data: list):\n    return None\n\n\n")
@@ -25,15 +25,15 @@ def create_day_stub(day: int, create_test_file: bool):
             fh.write(f"    problem = Problem({day})\n\n")
             fh.write("    print(part_a(load(problem)))\n")
             fh.write("    # print(part_b(load(problem)))\n\n")
-            fh.write("    # problem.submit(part_a(load()), 'a')\n")
-            fh.write("    # problem.submit(part_b(load()), 'b')\n")
+            fh.write("    # problem.submit(part_a(load(problem)), 'a')\n")
+            fh.write("    # problem.submit(part_b(load(problem)), 'b')\n")
 
     # Create test.in (optional)
     if create_test_file:
-        if os.path.exists(file_name := f'src/day{day}/test.in'):
+        if os.path.exists(file_name := f'src/{day}/test.in'):
             if not _prompt_override(file_name):
                 return
-        with open(f'src/day{day}/test.in', 'w'):
+        with open(f'src/{day}/test.in', 'w'):
             pass
 
 
