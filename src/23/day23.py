a97107ff94410db1c67f4939de9e4f10d7059e38
Lukas <games.luki@gmail.com>
2021-08-17 15:54:53 +0200

    Day 23: code improvements

--- src/23/day23.py
+++ src/23/day23.py
@@ -1,71 +1,63 @@
 from src.problem import Problem
-from collections import deque
 from funcy import lmap
-from itertools import cycle
+from math import prod
 import numpy as np
 
 
-def play(follow: dict, total_cups: int, moves: int):
-    current = list(follow.keys())[0]
+def play(nxt: dict, moves: int):
+    highest, cur = len(nxt), list(nxt.keys())[0]
 
     for _ in range(moves):
-        # "pick up" cups
-        pick_up = [_get_next(current, follow, i) for i in range(1, 4)]
-        follow[current] = _get_next(current, follow, 4)
+        # pick up and unlink cups
+        pick_up = (nxt_4 := list(traverse(nxt, start=cur, steps=4)))[0:3]
+        nxt[cur] = nxt_4[-1]
 
-        # get destination cup
-        destination = (current - 2) % total_cups + 1
-        while destination in pick_up:
-            destination = (destination - 2) % total_cups + 1
+        # find destination cup
+        dest = (cur - 2) % highest + 1
+        while dest in pick_up:
+            dest = (dest - 2) % highest + 1
 
-        # place cups clockwise to destination cup
-        follow[pick_up[-1]] = follow[destination]
-        follow[destination] = pick_up[0]
+        # link cups clockwise to destination cup
+        nxt[dest], nxt[pick_up[-1]] = pick_up[0], nxt[dest]
 
-        # select new current cup
-        current = follow[current]
-
-
-def _get_next(cur: int, follow: dict, n: int) -> int:
-    for _ in range(n):
-        cur = follow[cur]
-    return cur
+        # assign next current cup
+        cur = nxt[cur]
 
 
-def part_a(cups: list, follow: dict):
-    play(follow, total_cups=len(cups), moves=100)
-
-    result, current = '', 1
-    for _ in range(len(cups) - 1):
-        result += str(follow[current])
-        current = follow[current]
-
-    return result
+def traverse(nxt: dict, start: int, steps: int) -> iter:
+    for _ in range(steps):
+        start = nxt[start]
+        yield start
 
 
-def part_b(cups: list, follow: dict):
+def part_a(nxt: dict):
+    play(nxt, moves=100)
+    return ''.join(lmap(str, traverse(nxt, start=1, steps=len(nxt) - 1)))
+
+
+def part_b(nxt: dict):
     total_cups = 1_000_000
+    first, last = list(nxt.keys())[0], list(nxt.keys())[-1]
 
-    follow[cups[-1]] = len(cups) + 1
-    for i in range(len(cups) + 1, total_cups):
-        follow[i] = i % total_cups + 1
-    follow[total_cups] = cups[0]
+    # link new cups
+    nxt[last], nxt[total_cups] = len(nxt) + 1, first
+    for i in range(len(nxt), total_cups):
+        nxt[i] = i + 1
 
-    play(follow, total_cups=total_cups, moves=10_000_000)
-
-    return _get_next(1, follow, 1) * _get_next(1, follow, 2)
+    play(nxt, moves=10_000_000)
+    return prod(traverse(nxt, start=1, steps=2))
 
 
 def load(p: Problem):
     data = lmap(int, p.raw_data())
-    return [data, dict(zip(data, np.roll(data, -1)))]
+    return dict(zip(data, np.roll(data, -1)))
 
 
 if __name__ == '__main__':
     problem = Problem(23)
 
-    # print(part_a(*load(problem)))
-    # print(part_b(*load(problem)))
+    # print(part_a(load(problem)))  # 26354798
+    # print(part_b(load(problem)))  # 166298218695
 
-    # problem.submit(part_a(*load(problem)), 'a')
-    problem.submit(part_b(*load(problem)), 'b')
+    # problem.submit(part_a(load(problem)), 'a')
+    # problem.submit(part_b(load(problem)), 'b')
