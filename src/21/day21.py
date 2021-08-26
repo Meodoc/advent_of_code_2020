104c4fa4bdaa7323361fa91c8565f42e60d03fb9
Lukas <games.luki@gmail.com>
2021-08-26 16:32:24 +0200

    Day 21, part b

--- src/21/day21.py
+++ src/21/day21.py
@@ -1,18 +1,30 @@
 from src.problem import Problem
-from collections import defaultdict
 from itertools import chain
-from copy import deepcopy
 
 
 def part_a(foods: list):
-    allergens = set(chain.from_iterable([food[1] for food in foods]))
     ingredients = list(chain.from_iterable([food[0] for food in foods]))
+    allergen_map = map_allergens(foods)
+
+    no_allergen = [ingredient for ingredient in ingredients if ingredient not in allergen_map.keys()]
+    return len(no_allergen)
+
+
+def part_b(foods: list):
+    allergen_map = map_allergens(foods)
+
+    sorted_ = dict(sorted(allergen_map.items(), key=lambda item: item[1]))
+    return ','.join(sorted_.keys())
+
+
+def map_allergens(foods: list):
+    allergens = set(chain.from_iterable([food[1] for food in foods]))
 
     allergen_map = {}
-    while len(allergen_map) < len(allergens):
+    while len(allergens) > 0:
         for allergen in allergens:
             containing = [set(food[0]) for food in foods if allergen in food[1]]
-            ingredient = containing[0].intersection(*containing)
+            ingredient = set.intersection(*containing)
 
             if len(ingredient) == 1:
                 ingredient = ingredient.pop()
@@ -20,19 +32,14 @@ def part_a(foods: list):
                 for food in foods:
                     if ingredient in food[0]:
                         food[0].remove(ingredient)
-
-    no_allergen = [ingredient for ingredient in ingredients if ingredient not in allergen_map.keys()]
-    return len(no_allergen)
-
-
-def part_b(data: list):
-    return None
+        allergens.difference_update(allergen_map.values())
+    return allergen_map
 
 
 def load(p: Problem):
     foods = []
     for food in p.data():
-        ingredients = food.split(' (')[0].split(' ')
+        ingredients = food.split(' (')[0].split()
         allergens = food.split(' (')[1].replace('contains ', '').rstrip(')').split(', ')
         foods.append((ingredients, allergens))
     return foods
@@ -41,7 +48,7 @@ def load(p: Problem):
 if __name__ == '__main__':
     problem = Problem(21, test=False)
 
-    print(part_a(load(problem)))
+    # print(part_a(load(problem)))
     # print(part_b(load(problem)))
 
     # problem.submit(part_a(load(problem)), 'a')
